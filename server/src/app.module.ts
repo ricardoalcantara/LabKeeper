c7612c5e87913de29968dacb3653e077af0c6f25
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2025-01-02 02:50:12 +0000

    refactor: remove host-related modules, services, and DTOs as part of code cleanup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
-import { HostModule } from './host/host.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -11,9 +9,7 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
     }),
     HealthModule,
-    HostModule,
     DatabaseModule,
-    UsersModule,
   ],
 })
 export class AppModule {}
